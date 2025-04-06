dc7b0c20b2a60fc5972a41b893cc6a8b29274784
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-04-06 21:52:45 +0700

    ap dung mo hinh MVC voi nodejs

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,19 +1,11 @@
 import express, { Express } from "express";
+import { getCreateUserPage, getHomePage } from "../controllers/user.controller";
 
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-  router.get("/", (req, res) => {
-    res.render("home.ejs");
-  });
-
-  router.get("/tvhung", (req, res) => {
-    res.send("Hello tran van hung web");
-  });
-
-  router.get("/abc", (req, res) => {
-    res.send("Hello abc");
-  });
+  router.get("/", getHomePage);
+  router.get("/create-user", getCreateUserPage);
 
   app.use("/", router);
 };
