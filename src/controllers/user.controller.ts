6d37cee71e553fa9f3813d03fc6071871436ca6d
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-04-11 16:05:27 +0700

    tao moi user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,11 +12,11 @@ const getCreateUserPage = (req: Request, res: Response) => {
   return res.render("create-user");
 };
 
-const postCreateUser = (req: Request, res: Response) => {
+const postCreateUser = async (req: Request, res: Response) => {
   const { fullName, email, address } = req.body;
 
   // handle create user
-  handleCreateUser(fullName, email, address);
+  await handleCreateUser(fullName, email, address);
 
   return res.redirect("/");
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,26 @@
 import getConnection from "../config/database";
 
-const handleCreateUser = (fullName: string, email: string, address: string) => {
+const handleCreateUser = async (
+  fullName: string,
+  email: string,
+  address: string
+) => {
   // insert into database
+  const connection = await getConnection();
 
-  // return result
-  console.log(">>> insert a new user");
+  try {
+    const sql =
+      "INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)";
+    const values = [fullName, email, address];
+
+    const [result, fields] = await connection.execute(sql, values);
+
+    return result;
+  } catch (err) {
+    console.log(err);
+
+    return [];
+  }
 };
 
 const getAllUsers = async () => {
