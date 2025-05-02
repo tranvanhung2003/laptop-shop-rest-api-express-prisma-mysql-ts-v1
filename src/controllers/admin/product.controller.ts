aff07fb66e0311907e1658c8c5e732e6fc177f97
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-05-02 18:09:11 +0700

    hien thi thong bao loi

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -5,18 +5,44 @@ import {
 } from "src/validation/product.schema";
 
 const getAdminCreateProductPage = (req: Request, res: Response) => {
-  return res.render("admin/product/create");
+  const errors = [];
+  const oldData = {
+    name: "",
+    price: "",
+    detailDesc: "",
+    shortDesc: "",
+    quantity: "",
+    factory: "",
+    target: "",
+  };
+
+  return res.render("admin/product/create", { errors, oldData });
 };
 
 const postAdminCreateProduct = (req: Request, res: Response) => {
-  const { name } = req.body;
+  const { name, price, detailDesc, shortDesc, quantity, factory, target } =
+    req.body as TypeProductSchema;
 
-  try {
-    const result = ProductSchema.parse(req.body);
-  } catch (error) {
-    console.log(error);
+  const validate = ProductSchema.safeParse(req.body);
+
+  if (!validate.success) {
+    // error
+    const errorZod = validate.error.issues;
+    const errors = errorZod.map((item) => `${item.message} (${item.path[0]})`);
+    const oldData = {
+      name,
+      price,
+      detailDesc,
+      shortDesc,
+      quantity,
+      factory,
+      target,
+    };
+
+    return res.render("admin/product/create", { errors, oldData });
   }
 
+  // success
   return res.redirect("/admin/product");
 };
 
--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -1,8 +1,11 @@
-import { string, z } from "zod";
+import { number, string, z } from "zod";
 
 const ProductSchema = z.object({
   name: z.string().trim().min(1, { message: "Tên không được để trống" }),
-  price: z.number().min(1, { message: "Giá tiền tối thiếu là 1" }),
+  price: z
+    .string()
+    .transform((val) => (val === "" ? 0 : Number(val)))
+    .refine((num) => num > 0, { message: "Số tiền tối thiểu là 1" }),
   detailDesc: z
     .string()
     .trim()
@@ -11,8 +14,11 @@ const ProductSchema = z.object({
     .string()
     .trim()
     .min(1, { message: "shortDesc không được để trống" }),
-  quantity: z.number().min(1, { message: "Số lượng tối thiểu là 1" }),
-  factory: string().trim().min(1, { message: "factory không được để trống" }),
+  quantity: z
+    .string()
+    .transform((val) => (val === "" ? 0 : Number(val)))
+    .refine((num) => num > 0, { message: "Số lượng tối thiểu là 1" }),
+  factory: z.string().trim().min(1, { message: "factory không được để trống" }),
   target: z.string().trim().min(1, { message: "target không được để trống" }),
 });
 
