757f12f9b2d4fac649ee79dd1041b2c47e2ae39d
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-05-02 09:37:14 +0700

    validate voi zod

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from "express";
+import {
+  ProductSchema,
+  TypeProductSchema,
+} from "src/validation/product.schema";
 
 const getAdminCreateProductPage = (req: Request, res: Response) => {
   return res.render("admin/product/create");
 };
 
 const postAdminCreateProduct = (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  try {
+    const result = ProductSchema.parse(req.body);
+  } catch (error) {
+    console.log(error);
+  }
+
   return res.redirect("/admin/product");
 };
 
---
+++ src/validation/product.schema.ts
@@ -0,0 +1,21 @@
+import { string, z } from "zod";
+
+const ProductSchema = z.object({
+  name: z.string().trim().min(1, { message: "Tên không được để trống" }),
+  price: z.number().min(1, { message: "Giá tiền tối thiếu là 1" }),
+  detailDesc: z
+    .string()
+    .trim()
+    .min(1, { message: "detailDesc không được để trống" }),
+  shortDesc: z
+    .string()
+    .trim()
+    .min(1, { message: "shortDesc không được để trống" }),
+  quantity: z.number().min(1, { message: "Số lượng tối thiểu là 1" }),
+  factory: string().trim().min(1, { message: "factory không được để trống" }),
+  target: z.string().trim().min(1, { message: "target không được để trống" }),
+});
+
+type TypeProductSchema = z.infer<typeof ProductSchema>;
+
+export { ProductSchema, TypeProductSchema };
